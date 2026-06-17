2153d7075e2446b90e2a1d5207936bcf930a5c6d
AmosDc <amosdompak6@gmail.com>
2026-06-17 12:46:15 +0700

    Add ngrok-skip-browser-warning header to Axios client

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -9,7 +9,8 @@ const api = axios.create({
   baseURL: API_URL,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true'
   }
 });
 
